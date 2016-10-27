6a18683affbed8dab61aa279e24363112925a197
forvoid <172953249@qq.com>
2016-10-27 23:01:56 +0800

    drop alert

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ function getKey()
 {
     if(event.keyCode==13){
       var search = "https://www.baidu.com/s?wd=+"+document.getElementById('search').value;
-      alert(search);
+      // alert(search);
        window.location.href=search;
       // return true;
 
@@ -11,30 +11,29 @@ function getKey()
 function goBaidu(){
 
   var search = "https://www.baidu.com/s?wd=+"+document.getElementById('search').value;
-  alert(search);
+  // alert(search);
   window.location.href=search;
 }
 function goGoogle1(){
 
   var search = "http://www.acgn.ren/search?q=+"+document.getElementById('search').value;
-  alert(search);
   window.location.href=search;
 }
 function goGoogle2(){
 
   var search = "http://g.odbook.com/search?q=+"+document.getElementById('search').value;
-  alert(search);
+  // alert(search);
   window.location.href=search;
 }
 function goYunPan(){
 
   var search = "http://www.panc.cc/s/"+document.getElementById('search').value+"/td_0";
-  alert(search);
+  // alert(search);
   window.location.href=search;
 }
 function goGitHub(){
 
   var search = "https://github.com/search?q="+document.searchform.search.value;
-  alert(search);
+  // alert(search);
   window.location.href=search;
 }
