2c2c6bbe26dbde1714d6dd425c686571e32612a0
forvoid <172953249@qq.com>
2016-10-27 22:53:11 +0800

    service synchronize

--- search.js
+++ search.js
@@ -1,25 +1,34 @@
+function getKey()
+{
+    if(event.keyCode==13){
+      var search = "https://www.baidu.com/s?wd=+"+document.getElementById('search').value;
+      alert(search);
+       window.location.href=search;
+      // return true;
 
+    }
+}
 function goBaidu(){
 
-  var search = "https://www.baidu.com/s?wd=+"+document.searchform.search.value;
+  var search = "https://www.baidu.com/s?wd=+"+document.getElementById('search').value;
   alert(search);
   window.location.href=search;
 }
 function goGoogle1(){
 
-  var search = "http://www.acgn.ren/search?q=+"+document.searchform.search.value;
+  var search = "http://www.acgn.ren/search?q=+"+document.getElementById('search').value;
   alert(search);
   window.location.href=search;
 }
 function goGoogle2(){
 
-  var search = "http://g.odbook.com/search?q=+"+document.searchform.search.value;
+  var search = "http://g.odbook.com/search?q=+"+document.getElementById('search').value;
   alert(search);
   window.location.href=search;
 }
 function goYunPan(){
 
-  var search = "http://www.panc.cc/s/"+document.searchform.search.value+"/td_0";
+  var search = "http://www.panc.cc/s/"+document.getElementById('search').value+"/td_0";
   alert(search);
   window.location.href=search;
 }
